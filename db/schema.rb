625e73339f7e35d94d2ad2b2bd6310c8f8c14ba2
nonmadden <nonmadden@gmail.com>
2016-04-14 20:07:04 +0700

    update code

--- db/schema.rb
+++ db/schema.rb
@@ -51,4 +51,39 @@ ActiveRecord::Schema.define(version: 20160406144828) do
     t.datetime "updated_at"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.string   "url"
+    t.datetime "published_at"
+  end
+
+  add_index "posts", ["url"], name: "index_posts_on_url", unique: true, using: :btree
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
+
+  add_index "taggings", ["post_id"], name: "index_taggings_on_post_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
